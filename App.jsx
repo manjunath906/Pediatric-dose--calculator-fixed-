58dba97a9892b2cddda81fb26d057af417971e2d
manjunath906 <manjumylapur096@gmail.com>
2025-07-18 19:23:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import PediatricDoseCalculator from "./PediatricDoseCalculator";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-10">
+      <PediatricDoseCalculator />
+    </div>
+  );
+}
